54bcab1a917ca02790f7a38943f06df4c6fc7436
NicoNethy <5761@holbertonstudents.com>
2023-06-01 05:55:53 -0700

    task 7

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
-bbc = "{}{}"
-for i in range(0, 10):
-    for x in range(i + 1, 10):
-        print(bbc.format(i, x), end=", " if i < 8 else "\n")
+
+def comb3():
+    bbc = "{}{}"
+    for i in range(0, 10):
+        for x in range(i + 1, 10):
+            print(bbc.format(i, x), end=", " if i < 8 else "\n")
+
+
+from 6-print_comb3 import comb3
+
+comb3()
