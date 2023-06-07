41064d1cc7755e14a126df050dc385702a5a2a5d
NicoNethy <5761@holbertonstudents.com>
2023-06-07 12:45:54 -0700

    0

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -5,7 +5,4 @@ def print_list_integer(my_list=[]):
     if argc > 1:
         for arg in range(len(my_list[0:])):
             print("{}".format(my_list[arg]))
-
-
-my_list = [1, 2, 3, 4, 5]
-print_list_integer(my_list)
+            
