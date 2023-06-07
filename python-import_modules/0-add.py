ad21e5715e607aa514ff295f58549881082c5693
NicoNethy <5761@holbertonstudents.com>
2023-06-07 09:32:20 -0700

    fabi

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-bbc = "{} + {} = {}"
 if __name__ == "__main__":
     from add_0 import add
     a = 1
     b = 2
     coca = add(a, b)
-    print(bcc.format(a, b, coca))
+    bbc = "{} + {} = {}"
+    print(bbc.format(a, b, coca))
