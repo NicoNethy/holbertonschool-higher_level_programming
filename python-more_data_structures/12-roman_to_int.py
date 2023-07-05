e06cbdb7e3210ba30c9864ea225fea1e74b2da0e
NicoNethy <5761@holbertonstudents.com>
2023-07-04 21:01:08 -0700

    last

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,44 @@
 #!/usr/bin/python3
-def common_elements(set_1, set_2):
-    s1, s2, comm = list(set(set_1)), list(set(set_2)), []
-    for x in range(len(s1)):
-        for y in range(len(s2)):
-            if s1[x] == s2[y]:
-                comm.append(s1[x])
-    return comm
+def whatRoman(i):
+    if i == 'I':
+        return 1
+    elif i == 'V':
+        return 5
+    elif i == 'X':
+        return 10
+    elif i == 'L':
+        return 50
+    elif i == 'C':
+        return 100
+    elif i == 'D':
+        return 500
+    elif i == 'M':
+        return 1000
+    else:
+        return 0
+
+
+def roman_to_int(roman_string):
+    if roman_string and isinstance(roman_string, str):
+        result = list(map(whatRoman, roman_string))
+    else:
+        return 0
+    x, y = '', 0
+    letterI = {'I': 0, 'V': -2, 'X': -2, 'L': -2, 'C': -2, 'D': -2, 'M': -2}
+    letterX = {'I': 0, 'V': 0, 'X': 0, 'L': -20, 'C': -20, 'D': -20, 'M': -20}
+    letterC = {'I': 0, 'V': 0, 'X': 0, 'L': 0, 'C': 0, 'D': -200, 'M': -200}
+
+    for let in roman_string:
+        if x == 'I':
+            y += letterI[let]
+        elif x == 'X':
+            y += letterX[let]
+        elif x == 'C':
+            y += letterC[let]
+        x = let
+
+    for x in result:
+        if x == 0:
+            return 0
+        y += x
+    return y
