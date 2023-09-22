dcf449fe5eff8a685314a183446a06c50fa975e5
NicoNethy <5761@holbertonstudents.com>
2023-09-22 07:36:34 -0700

    the add

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -3,7 +3,4 @@ if __name__ == "__main__":
     from add_0 import add
     a = 1
     b = 2
-    coca = add(a, b)
-    bbc = "{} + {} = {}"
-    print(bbc.format(a, b, coca))
-    
+    print("{} + {} = {}".format(a, b, add(a, b)))
