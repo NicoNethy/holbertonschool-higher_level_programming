bbb5228c8ca14a19434ae1ce1a27f68de3568543
NicoNethy <5761@holbertonstudents.com>
2023-10-03 15:24:17 -0700

    all the files empty, the 0 completed and the readme created from the shards of adonalsium

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+'''add_integer module'''
+
+
 def add_integer(a, b=98):
-    if not isinstance(a, (int, float)):
+    '''add integers'''
+    if type(a) != int and type(a) != float:
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
+    if type(b) != int and type(b) != float:
         raise TypeError("b must be an integer")
-    a = int(a) if isinstance(a, float) else a
-    b = int(b) if isinstance(b, float) else b
-    return a + b
+    return int(a + b)
