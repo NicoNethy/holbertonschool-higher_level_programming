cea038ca25c9c106c5dc65353db146dd6c911bd5
NicoNethy <5761@holbertonstudents.com>
2023-10-03 16:00:28 -0700

    5

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-'''print sqaure module'''
+def text_indentation(text):
+    if text == "":
+        return
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
 
-
-def print_square(size):
-    '''prints a square with the character #'''
-    if type(size) != int:
-        raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >= 0")
-    if type(size) == float and size < 0:
-        raise TypeError("size must be an integer")
-    for i in range(size):
-        print("{}".format("#" * size))
+    x = ''
+    for i in text:
+        if i != ' ' and (x != '.' and x != '?' and x != ':'):
+            print(x, end="")
+        elif i == ' ':
+            print(x, end="")
+        else:
+            print("{}\n".format(x))
+        x = i
+    print(i, end="")
