90fcc20fea3fb5df44b69d1ed148c7daf861a826
NicoNethy <5761@holbertonstudents.com>
2023-11-17 05:23:14 -0800

    task 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -10,8 +10,10 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE\
-        BINARY name = %(name)s", {'name': sys.argv[4]})
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
+    cur.execute("SELECT states.id, states.name FROM states".format())
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        if row[1] == sys.argv[4]:
+            print(row)
+    cur.close()
+    db.close()
